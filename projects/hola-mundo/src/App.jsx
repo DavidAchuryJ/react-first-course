bb961e55ef945a22652e195134405bb16918840a
david fernando achury <davidachury123@gmail.com>
2024-10-10 14:08:19 -0500

    primer proyecto con react

---
+++ projects/hola-mundo/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
+
+export function App() {
+  return (
+    <section className='App'>
+      <TwitterFollowCard userName='midudev' initialIsFollowing={true}>
+        Miguel Ángel Durán
+      </TwitterFollowCard>
+
+      <TwitterFollowCard userName='pheralb'>Pablo Hernandez</TwitterFollowCard>
+    </section>
+  );
+}
